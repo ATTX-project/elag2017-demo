41a60ba35e4eebeb437232b3b7c653996def910a
João M. da Silva <joao.silva@helsinki.fi>
2017-05-30 14:49:12 +0300

    Updated script and yml for demo

--- docker-machine-Swarm.sh
+++ docker-machine-Swarm.sh
@@ -1,21 +1,21 @@
 for i in 1 2 3; do
-  docker-machine create -d virtualbox --virtualbox-memory "2048" node-$i
+  docker-machine create -d virtualbox --virtualbox-memory "3072" attx-swarm-$i
 done
 
-eval $(docker-machine env node-1)
+eval $(docker-machine env attx-swarm-1)
 
 docker swarm init \
-  --advertise-addr $(docker-machine ip node-1)
+  --advertise-addr $(docker-machine ip attx-swarm-1)
 
 TOKEN=$(docker swarm join-token -q worker)
 
 for i in 2 3; do
-  eval $(docker-machine env node-$i)
+  eval $(docker-machine env attx-swarm-$i)
 
   docker swarm join \
     --token $TOKEN \
-    --advertise-addr $(docker-machine ip node-$i) \
-    $(docker-machine ip node-1):2377
+    --advertise-addr $(docker-machine ip attx-swarm-$i) \
+    $(docker-machine ip attx-swarm-1):2377
 done
 
 echo ">> The Docker Machine Swarm is up and running"
